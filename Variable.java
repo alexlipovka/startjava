d381cbbb82aa1721495de9ff37422a46fdc7de33
DinSV <DinSV@users.noreply.github.com>
2020-12-16 01:06:25 +0700

    16.12.2020
    first home work

---
+++ Variable.java
@@ -0,0 +1,14 @@
+package dinsv.course.startjava.home1;
+
+public class MyFirstApp {
+	public static void main(String[] args) {
+		byte windows = 10;
+		short kernels = 4;
+		int drd = 16;
+		long hdd = 1700l;
+		float cpuFrequency = 3.1f;
+		double quantityMonitors = 2.0;
+		char quantityPc = 1;
+		boolean work = true;
+	}
+}
