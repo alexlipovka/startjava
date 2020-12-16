1bead3faf7dc119f32e0f1e8a302305f675d0fe3
DinSV <DinSV@users.noreply.github.com>
2020-12-16 22:47:33 +0700

    Lesson1
    Variable.java corrected mistakes

---
+++ dinsv/startjava/lesson1/Variable.java
@@ -0,0 +1,21 @@
+package dinsv.startjava.lesson1;
+
+public class Variable {
+	public static void main(String[] args) {
+
+		byte windows = 10;
+		short kernels = 4;
+		int ddr = 16;
+		long hdd = 1700l;
+		float cpuFrequency = 3.1f;
+		double quantityMonitors = 2.0;
+		char quantityPc = '1';
+		boolean work = true;
+
+		System.out.println("i have " + quantityPc + " PC");
+		System.out.println("My PC parametrs: cpu " + kernels + " kernels / Frequency " + cpuFrequency
+				+ "Mhz / RAM DDR3 " + ddr + "Gb / hard drive " + hdd + "Gb");
+		System.out.println(
+				quantityMonitors + " monitors connected, OS Windows " + windows + ". End it work - " + work + ". =)");
+	}
+}
