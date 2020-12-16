183350b28f4e2ac3f628c96bf8e33d7ee5c8d512
Constantin_S <63643468+DinSV@users.noreply.github.com>
2020-12-17 00:03:15 +0700

    Update Variable.java
    update

--- lesson1/Variable.java
+++ lesson1/Variable.java
@@ -2,7 +2,6 @@ package lesson1;
 
 public class Variable {
 	public static void main(String[] args) {
-
 		byte windows = 10;
 		short kernels = 4;
 		int ddr = 16;
@@ -15,7 +14,6 @@ public class Variable {
 		System.out.println("i have " + quantityPc + " PC");
 		System.out.println("My PC parametrs: cpu " + kernels + " kernels / Frequency " + cpuFrequency
 				+ "Mhz / RAM DDR3 " + ddr + "Gb / hard drive " + hdd + "Gb");
-		System.out.println(
-				quantityMonitors + " monitors connected, OS Windows " + windows + ". End it work - " + work + ". =)");
+		System.out.println(quantityMonitors + " monitors connected, OS Windows " + windows + ". End it work - " + work + ". =)");
 	}
 }
