4381b77cb3c39849e8c63c92d16ec802e7da038a
DinSV <dinscalin.ks@gmail.com>
2020-12-21 13:03:46 +0700

    recast

--- lesson1/MyFirstGame.java
+++ lesson1/MyFirstGame.java
@@ -2,22 +2,19 @@ package lesson1;
 
 public class MyFirstGame {
 	public static void main(String[] args) {
-		int secretX = 76;
-		int numberY = 3;
-
+		int secretX = 369;
+		int numberY = 30;
+		
+		while (secretX != numberY) {
 		if(numberY < secretX) {
-		while(numberY < secretX) {
 			System.out.println("You entered the number is less than what the computer guessed");
 			System.out.println(numberY);
-			numberY = numberY + 6;
-		} 
-	}	else if(numberY > secretX) {
-		while(numberY > secretX) {
+			numberY = numberY + 53;
+		} 	if(numberY > secretX) {
 			System.out.println("You entered the number is more than what the computer guessed");
 			System.out.println(numberY);
-			numberY = numberY - 1;
-		}
-	}	else if(numberY == secretX){
+			numberY = numberY - 5;
+	}}	if(numberY == secretX){
 		System.out.println("You Win");
 		System.out.println(numberY);
 	}
