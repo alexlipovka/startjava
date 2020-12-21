8700027e4892c55a85e015ab0b2ad5d9513a4aa2
DinSV <dinscalin.ks@gmail.com>
2020-12-21 12:03:09 +0700

    mod

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -4,30 +4,25 @@ public class Calculator {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 5;
-		char operation = '^';
+		char operation = '*';
 		int result = 1;
 
 		if(operation == '+') {
 			result = a + b;
-			System.out.println("your result = " + result);
 		} else if(operation == '-') {
 			result = a - b;
-			System.out.println("your result = " + result);
 		} else if(operation == '*') {
 			result = a * b;
-			System.out.println("your result = " + result);
 		} else if(operation == '/') {
 			result = a / b;
-			System.out.println("your result = " + result);
 		} else if(operation == '%') {
 			result = a % b;
-			System.out.println("your result = " + result);
 		} else if(operation == '^') {
 			while(b >= 1) {
 				result = result * a;
 				b--;
 			}
-		System.out.println("your result = " + result);
 		}
+		System.out.println("your result = " + result);
 	}
 }
