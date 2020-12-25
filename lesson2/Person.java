f0e0445384d3d4ae489f3345261d7374d462704a
DinSV <DinSV@users.noreply.github.com>
2020-12-25 09:12:26 +0700

    Update Person.java
    format and method

--- lesson2/Person.java
+++ lesson2/Person.java
@@ -1,14 +1,37 @@
-package lesson2;
-
 public class Person {
-	
+
 	String name = "Bill";
 	String heirColor = "white";
 	int age = 65;
 	float weight = 75f;
 	float height = 175f;
 	char sex = 'M';
-	
+
+	void go() {
+		System.out.println(name + " go");
+	}
+
+	boolean sit() {
+		return true;
+	}
+
+	String talk() {
+		String talk = "Hello!";
+		return talk;
+	}
+
+   void run() {
+		System.out.println("I'm old for this");
+
+	}
+
+	void learnJava() {
+		System.out.println("Bill is studying Java");
+
+	}
+
+/* Bill все равно поедет на рыбалку
+	когда будет хорошая погода
 	public void hello() {
 		System.out.println("Hello!");
 	}
@@ -24,6 +47,6 @@ public class Person {
 	}
 		public String say(String aWord) {
 		String answerBill = "Ok, no problem" + aWord;
-		return answerBill;		
-	}
+		return answerBill;
+	}*/
 }
