34a18d082e95fcdf8b13818d92d8520236c89d79
DinSV <DinSV@users.noreply.github.com>
2020-12-28 20:56:01 +0700

    format

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,85 +1,86 @@
 public class Wolf {
-  
-  private String sex;
-  private String name;
-  private double weight;
-  private int age;
-  private String color;
-  private int prey; // добыча для реализации метода охота
 
-  public void go() {
-    System.out.println(name + " go");
-  }
+    private String sex;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
+    private int prey; // добыча для реализации метода охота
 
-  public void sit() {
-    System.out.println(name + " sit");
-  }
-
-  public void run() {
-    System.out.println(name + " run");
-  }
-
-  public String yowl() {
-    String yowl = "ouuuu";
-    return yowl;
-  }
-
-  public boolean hunts() {
-    if(prey > 0) {
-      return true;
-      } else {
-      return false;
+    public void go() {
+      System.out.println(name + " go");
     }
-  }
 
-  public String getName() {
-      return name;
-  } 
- 
-  public void setName(String newName) {
-      name = newName;
-  }
-
- public String getSex() {
-      return sex;
-  } 
- 
-  public void setSex(String variableSex) {
-      sex = variableSex;
-  }
-
-  public double getWeight() {
-      return weight;
-  } 
- 
-  public void setWeight(double numeralWeight) {
-      weight = numeralWeight;
-  }
-
-  public int getAge() {
-      return age;
-  } 
- 
-  public void setAge(int variableAge) {
-      if(variableAge > 8) {
-        System.out.println("Incorrect age!");
-      } else {
-      age = variableAge;
+    public void sit() {
+      System.out.println(name + " sit");
     }
-  }
-  public String getColor() {
-      return color;
-  } 
+
+    public void run() {
+      System.out.println(name + " run");
+    }
+
+    public String yowl() {
+      String yowl = "ouuuu";
+      return yowl;
+    }
+
+    public boolean hunts() {
+        if(prey > 0) {
+            return true;
+        }   else {
+            return false;
+        }
+    }
+
+    public String getName() {
+       return name;
+    }  
  
-  public void setColor(String newColor) {
-      color = newColor;
-  }
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getSex() {
+        return sex;
+    } 
+ 
+    public void setSex(String variableSex) {
+        sex = variableSex;
+    }
+
+    public double getWeight() {
+        return weight;
+    } 
+ 
+    public void setWeight(double numeralWeight) {
+        weight = numeralWeight;
+    }
+
+    public int getAge() {
+        return age;
+    } 
+ 
+    public void setAge(int variableAge) {
+        if(variableAge > 8) {
+          System.out.println("Incorrect age!");
+        } else {
+          age = variableAge;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    } 
+ 
+    public void setColor(String newColor) {
+        color = newColor;
+    }
   
-  public int getPrey() {
-      return prey;
-  } 
+    public int getPrey() {
+        return prey;
+    } 
  
-  public void setPrey(int amountPrey) {
-      prey = amountPrey;
-  } 
+    public void setPrey(int amountPrey) {
+        prey = amountPrey;
+    } 
 }
--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -1,28 +1,28 @@
 public class WolfTest {
-  public static void main(String[] args) {
-    Wolf wolf1 = new Wolf();
+    public static void main(String[] args) {
+        Wolf wolf1 = new Wolf();
 
-    wolf1.setName("Volchok");
-    wolf1.setSex("male");
-    wolf1.setWeight(15.7);
-    wolf1.setAge(7);
-    wolf1.setColor("Pink");
-    wolf1.setPrey(0);
+        wolf1.setName("Volchok");
+        wolf1.setSex("male");
+        wolf1.setWeight(15.7);
+        wolf1.setAge(7);
+        wolf1.setColor("Pink");
+        wolf1.setPrey(0);
    
-    System.out.println(wolf1.getName());
-    System.out.println(wolf1.getSex());
-    System.out.println(wolf1.getWeight());
-    System.out.println(wolf1.getAge());
-    System.out.println(wolf1.getColor());
-    wolf1.run();
+        System.out.println(wolf1.getName());
+        System.out.println(wolf1.getSex());
+        System.out.println(wolf1.getWeight());
+        System.out.println(wolf1.getAge());
+        System.out.println(wolf1.getColor());
+        wolf1.run();
 
-    if(wolf1.hunts() == true) {
-        System.out.println(wolf1.getName() + " following the trail"); 
+        if(wolf1.hunts() == true) {
+            System.out.println(wolf1.getName() + " following the trail"); 
         }
-    else { 
-        System.out.println(wolf1.getName() + " did not find animals");
+        else { 
+            System.out.println(wolf1.getName() + " did not find animals");
         }
-    System.out.println(wolf1.yowl());
+        System.out.println(wolf1.yowl());
     }
 }
 
