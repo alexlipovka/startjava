8614cd0a79fcf6cdcc5a8f891a6f24cc49372e03
Constantin_S <63643468+DinSV@users.noreply.github.com>
2021-01-13 23:30:01 +0700

    1

--- lesson2/Calculator.java
+++ lesson2/Calculator.java
@@ -1,31 +1,31 @@
 package lesson2;
 
 public class Calculator {
-	private int a, b;			// переменные с которыми будут производится действия
-	private char operation; // действие
-	private int result = 1;
-	private boolean restart = true; // для проверки рестарт или завершение
-	private String answer; // ответ пользователя 
+    private int a, b;			// переменные с которыми будут производится действия
+    private char operation; // действие
+    private int result = 1;
+    private boolean restart = true; // для проверки рестарт или завершение
+    private String answer; // ответ пользователя 
 	
-	switch(input.operation) {
-		case '+': result = a + b;
-			break;
-		case '-': result = a - b;
-			break;
-		case '*': result = a * b;
-			break;
-		case '/': result = a / b;
-			break;
-		case '%': result = a % b;
-			break;
-		case '^': while(b >= 1) {
-			result *= a;
-			b--;
-			} 
-	}
+    switch(input.operation) {
+	case '+': result = a + b;
+		break;
+	case '-': result = a - b;
+		break;
+	case '*': result = a * b;
+		break;
+	case '/': result = a / b;
+		break;
+	case '%': result = a % b;
+		break;
+	case '^': while(b >= 1) {
+		result *= a;
+		b--;
+		} 
+    }
 
-	public void calcResult() {
-		System.out.println("your result = " + result);	
+    public void calcResult() {
+	    System.out.println("your result = " + result);	
 	}
 
 	public boolean restart(String ansver) {
