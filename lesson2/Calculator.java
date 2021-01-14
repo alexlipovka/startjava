a2089b13acec86beb6fbfd6f605699d6a816069e
Constantin_S <63643468+DinSV@users.noreply.github.com>
2021-01-14 13:54:34 +0700

    upd

--- lesson2/Calculator.java
+++ lesson2/Calculator.java
@@ -1,53 +1,74 @@
 package lesson2;
 
 public class Calculator {
-    private int a, b;			// переменные с которыми будут производится действия
+    private int a, b;           // переменные с которыми будут производится действия
     private char operation; // действие
     private int result = 1;
     private boolean restart = true; // для проверки рестарт или завершение
     private String answer; // ответ пользователя 
-	
-    switch(input.operation) {
-	case '+': result = a + b;
-	    break;
-	case '-': result = a - b;
-	    break;
-	case '*': result = a * b;
-	    break;
-	case '/': result = a / b;
-	    break;
-	case '%': result = a % b;
-	    break;
-	case '^': while(b >= 1) {
-	    result *= a;
-	    b--;
-	    }
+    
+    public void setResult(char operation) {
+        switch(this.operation) {
+            case '+': result = a + b;
+                break;
+            case '-': result = a - b;
+                break;
+            case '*': result = a * b;
+                break;
+            case '/': result = a / b;
+               break;
+            case '%': result = a % b;
+                break;
+            case '^': while(b >= 1) {
+                result *= a;
+                b--;
+                }
+        }
     }
 
-    public void calcResult() {
-        System.out.println("your result = " + result);	
+    public int getResult() {
+        return result;
     }
 
-    public boolean restart(String ansver) {
-	if(answer == "y") {
-	    return true;
-	} else if(answer == "n") {
-	} return false;
+    public void printResult() {
+        System.out.println("your result = " + result);  
+    }
+
+    public boolean setRestart(String answer) {
+        if(this.answer == "y") {
+            return true;
+        } else if(this.answer == "n") {
+        } return false;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public char getOperation() {
+        return operation;
+    }
+    public void setOperation(char operation) {
+        this.operation = operation;
     }
 
     public void setA(int a) {
-	a = this.a;
+        a = this.a;
     }
 
     public int getA() {
-	return a;
+        return a;
     }
 
     public void setB(int b) {
-	b = this.b;
+        b = this.b;
     }
 
     public int getB() {
-	return b;
+        return b;
     }
 }
