6ff5064b02cfe9ebf3787da74f18e3e7ba7161d1
Constantin_S <63643468+DinSV@users.noreply.github.com>
2021-02-03 23:24:17 +0700

    Update Calculator.java

--- lesson2/Calculator.java
+++ lesson2/Calculator.java
@@ -4,6 +4,18 @@ public class Calculator {
     private char operation;
     long result;
 
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
     public void calculate() {
         switch(this.operation) {
             case '+':
@@ -28,17 +40,5 @@ public class Calculator {
                 }
                 System.out.println("Your result = " + result);    
         }
-    }
-
-   public void setOperation(char operation) {
-        this.operation = operation;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public void setB(int b) {
-        this.b = b;
     }
 }
