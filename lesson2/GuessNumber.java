4c1d59a8a670a8bb607ffbb6435628365b903abb
Constantin_S <63643468+DinSV@users.noreply.github.com>
2021-02-05 14:35:21 +0700

    жесть какая то

--- lesson2/GuessNumber.java
+++ lesson2/GuessNumber.java
@@ -4,14 +4,23 @@ public class GuessNumber {
     Player player1;
     Player player2;
 
-    do {
+    do { // ввод числа первого игрока
         if(player1.inputNumber == answer) {
             System.out.println(player1.name + " Win!");
             break;
-        }   else if(player2.inputNumber == answer) {
-            System.out.println(player1.name + " Win!");
+        } else if(player1.inputNumber < answer) {
+            System.out.println("ваше число меньше загаданного");
+        } else if(player1.inputNumber > answer) {
+            System.out.println("ваше число больше загаданного");
+        // else вводит число следующий игрок
+        } else if(player2.inputNumber == answer) {
+            System.out.println(player2.name + " Win!");
             break;
-        }
+        } else if(player1.inputNumber < answer) {
+            System.out.println("ваше число меньше загаданного");
+        } else if(player1.inputNumber > answer) {
+            System.out.println("ваше число больше загаданного");
+        } // else цикл с начала
         } while (player1.inputNumber != answer && player2.inputNumber != answer);
 
     public void setAnswer(int answer) {
@@ -20,4 +29,5 @@ public class GuessNumber {
 
     public void setInputNumber(int inputNumber) {
         this.inputNumber = inputNumber;
+    }
 }
--- lesson2/GuessNumberTest.java
+++ lesson2/GuessNumberTest.java
@@ -6,11 +6,11 @@ public class GuessNumberTest {
         Scanner input = new Scanner(System.in);
         Player player1 = new Player(input.nextLine());
         Player player2 = new Player(input.nextLine());
-        System.out.println("Первый игрок "+ player1.getName() + " "+ "Первый игрок " + player2.getName());
+        System.out.println("Первый игрок "+ player1.getName() + " "+ "Второй игрок " + player2.getName());
 
         System.out.println("угадайте число");
         Random randomInt = new Random();
-        final int answer = randomInt.nextInt(101);
-        randonInt.setAnswer(answer);
+        //int answer = randomInt.nextInt(101);
+        randomInt.setAnswer(randomInt.nextInt(101)); // и херли надо то, еще один класс лепить чтоль
     }
 }
