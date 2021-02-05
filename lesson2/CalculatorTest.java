5247d5c950ee081286c9aee463bb82a629ff8351
Constantin_S <63643468+DinSV@users.noreply.github.com>
2021-02-05 10:50:30 +0700

    Update CalculatorTest.java

--- lesson2/CalculatorTest.java
+++ lesson2/CalculatorTest.java
@@ -1,32 +1,10 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
-    private boolean restart;
-    private char answer;
-    public boolean setRestart(char answer) {
-        if(answer == 'y') {
-            return restart = true;
-        } else if(answer == 'n') {
-        } return restart = false;
-    }
-
-    public boolean getRestart() {
-        return restart;
-    }
-    
-    public char getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(char answer) {
-        this.answer = answer;
-    }
-    
     public static void main(String[] args) {
-        CalculatorTest answeCycle = new CalculatorTest();
+        char playerAnswer;
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
-                
         do {
             System.out.println("Enter first number");
             calc.setA(scan.nextInt());
@@ -41,19 +19,13 @@ public class CalculatorTest {
             scan.reset();
 
             System.out.println("want to use the program again? enter y / n ");
-            char playerAnswer;
             do {
                 playerAnswer = scan.next().charAt(0);
-                answeCycle.setAnswer(playerAnswer);
-                if(playerAnswer == 'y') {
-                    answeCycle.setRestart(playerAnswer);
-                } else if(playerAnswer == 'n') {
-                    answeCycle.setRestart(playerAnswer);
-                } else {
+                if(playerAnswer != 'y' && playerAnswer != 'n') {
                     System.out.println("you entered an invalid value, please try again");
                 }
-            } while(answeCycle.getAnswer() != 'y' && answeCycle.getAnswer() != 'n');
-        } while(answeCycle.getRestart());
+            } while(playerAnswer != 'y' && playerAnswer != 'n');
+        } while(playerAnswer == 'y');
         scan.close();
     }
 }
