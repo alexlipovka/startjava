23e1f733d97fa3e56e72042b94c99421d45dff47
DinSV <dinscalin.ks@gmail.com>
2021-02-07 21:47:47 +0700

    upd

--- lesson2/GuessNumber.java
+++ lesson2/GuessNumber.java
@@ -1,34 +1,11 @@
 public class GuessNumber {
-    пока пишу в 2х классах
-    /*private int inputNumber;
-    private int answer;
-    Player player1;
-    Player player2;
+    private int guessNumber;
 
-    do { // ввод числа первого игрока
-        if(player1.inputNumber == answer) {
-            System.out.println(player1.name + " Win!");
-            break;
-        } else if(player1.inputNumber < answer) {
-            System.out.println("ваше число меньше загаданного");
-        } else if(player1.inputNumber > answer) {
-            System.out.println("ваше число больше загаданного");
-        // else вводит число следующий игрок
-        } else if(player2.inputNumber == answer) {
-            System.out.println(player2.name + " Win!");
-            break;
-        } else if(player1.inputNumber < answer) {
-            System.out.println("ваше число меньше загаданного");
-        } else if(player1.inputNumber > answer) {
-            System.out.println("ваше число больше загаданного");
-        } // else цикл с начала
-        } while (player1.inputNumber != answer && player2.inputNumber != answer);
-
-    public void setAnswer(int answer) {
-        this.answer = answer;
+    public void setGuessNumber (int guessNumber) {
+        this.guessNumber = guessNumber;
     }
 
-    public void setInputNumber(int inputNumber) {
-        this.inputNumber = inputNumber;
+    public int getGuessNumber () {
+        return guessNumber;
     }
-}*/
+}
--- lesson2/GuessNumberTest.java
+++ lesson2/GuessNumberTest.java
@@ -3,44 +3,48 @@ import java.util.Random;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        //int answer;
+        char playerAnswer;
         Scanner input = new Scanner(System.in);
         System.out.println("input first player name ");
         Player player1 = new Player(input.nextLine());
         System.out.println("input second player name ");
         Player player2 = new Player(input.nextLine());
         System.out.println("First player "+ player1.getName() + " "+ "second player " + player2.getName());
-
         System.out.println("Guess the number Start");
         Random randomInt = new Random();
         int answer = randomInt.nextInt(101);
-        //randomInt.setAnswer(randomInt.nextInt(101)); // и херли надо то, еще один класс лепить чтоль
-
         do {
-            System.out.println(player1.getName() + " guess the number from 0 to 100");
-            player1.setInputNumber(input.nextInt());
-            if(player1.getInputNumber() == answer) {
-                System.out.println(player1.getName() + " Win!");
-                break;
-            } else if(player1.getInputNumber() < answer) {
-                System.out.println("your number is less than the guesswork");
-            } else if(player1.getInputNumber() > answer) {
-                System.out.println("your number is greater than the envisioned");
-            }
+            do {
+                System.out.println(player1.getName() + " guess the number from 0 to 100");
+                player1.setInputNumber(input.nextInt());
+                if(player1.getInputNumber() == answer) {
+                    System.out.println(player1.getName() + " Win!");
+                    break;
+                } else if(player1.getInputNumber() < answer) {
+                    System.out.println("your number is less than the guesswork");
+                } else if(player1.getInputNumber() > answer) {
+                    System.out.println("your number is greater than the envisioned");
+                }
 
-            System.out.println(player2.getName() + " guess the number from 0 to 100");
-            player2.setInputNumber(input.nextInt());
-            if(player2.getInputNumber() == answer) {
-                System.out.println(player2.getName() + " Win!");
-                break;
-            } else if(player2.getInputNumber() < answer) {
-                System.out.println("your number is less than the guesswork");
-            } else if(player2.getInputNumber()> answer) {
-                System.out.println("your number is greater than the envisioned");
-            } // else цикл с начала
-        } while (player1.getInputNumber() != answer && player2.getInputNumber() != answer);
-
-   /* тут будет - цикл хотите поиграть еще
-    }*/
+                System.out.println(player2.getName() + " guess the number from 0 to 100");
+                player2.setInputNumber(input.nextInt());
+                if(player2.getInputNumber() == answer) {
+                    System.out.println(player2.getName() + " Win!");
+                    break;
+                } else if(player2.getInputNumber() < answer) {
+                    System.out.println("your number is less than the guesswork");
+                } else if(player2.getInputNumber()> answer) {
+                    System.out.println("your number is greater than the envisioned");
+                } 
+            } while (player1.getInputNumber() != answer && player2.getInputNumber() != answer);
+            System.out.println("want to use the program again? enter y / n ");
+                do {
+                    playerAnswer = input.next().charAt(0);
+                    if(playerAnswer == 'n' || playerAnswer == 'y') {
+                        break;
+                    } else System.out.println("you entered an invalid value, please try again");
+                } while(playerAnswer != 'y' && playerAnswer != 'n');
+        } while(playerAnswer == 'y');
+        input.close();
     }
 }
